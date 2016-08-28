9eb0986a5cb00eba20219c48759a57b58ec5bfcb
Daniel Marquez <DannyBrianMarquez@gmail.com>
2016-08-28 00:18:34 -0400

    refactor: Remove FSL from pizza resize loop

--- views/js/main.js
+++ views/js/main.js
@@ -449,10 +449,11 @@ var resizePizzas = function(size) {
 
   // Iterates through pizza elements on the page and changes their widths
   function changePizzaSizes(size) {
-    for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
-      var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
-      var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
-      document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
+    var pizzaArray = document.querySelectorAll(".randomPizzaContainer");
+    var dx = determineDx(pizzaArray[0], size);
+    var newwidth = (pizzaArray[0].offsetWidth + dx) + 'px';
+    for (var i = 0; i < pizzaArray.length; i++) {
+      pizzaArray[i].style.width = newwidth;
     }
   }
 
